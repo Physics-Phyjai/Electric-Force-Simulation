ac1fd210199a33d215ac627ee9f96ccd22e95535
GGolfz <wisarutgolf11@gmail.com>
2022-11-09 21:28:29 +0700

    format

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -1,9 +1,9 @@
 const to2Decimal = (num: number) => {
-    return Math.round(num * 100) / 100;
-}
+  return Math.round(num * 100) / 100;
+};
 
 const toPointFive = (num: number) => {
-    return Math.round(num * 2) / 2;
-}
+  return Math.round(num * 2) / 2;
+};
 
-export {to2Decimal, toPointFive}
+export { to2Decimal, toPointFive };
