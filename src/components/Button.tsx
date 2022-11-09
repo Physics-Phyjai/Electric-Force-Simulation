4bcb0a4f2691bd205ad1059c0e429a4645ea06fb
GGolfz <wisarutgolf11@gmail.com>
2022-11-09 21:24:01 +0700

    update: button style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 const Button = (props: ButtonProps) => {
     return (
         <div className={`${styles.button} ${props.outlined ? styles.outlined : ''}`} onClick={props.onClick}>
-            {props.icon && <img src="images/carbon_add-alt.png" width="16px" height="16px" alt="add-icon" />}
+            {props.icon && <img src="images/carbon_add-alt.png" width="24px" height="24px" alt="add-icon" />}
             <p>{props.text}</p>
         </div>
     )
