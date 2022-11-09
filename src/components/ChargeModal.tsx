535aa482dcdd983707a11059edcb86bc3bd45413
GGolfz <wisarutgolf11@gmail.com>
2022-11-09 21:46:06 +0700

    update: modal style

--- src/components/ChargeModal.tsx
+++ src/components/ChargeModal.tsx
@@ -1,16 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { SketchPicker } from 'react-color';
-import ReactModal from 'react-modal';
-import { Charge } from '../type/charge';
-import InputText from './InputText';
-import SliderInput from './SliderInput';
-import Button from './Button';
-import { Force } from '../type/force';
+import React, { useEffect, useState } from "react";
+import { SketchPicker } from "react-color";
+import ReactModal from "react-modal";
+import { Charge } from "../type/charge";
+import InputText from "./InputText";
+import SliderInput from "./SliderInput";
+import Button from "./Button";
+import { Force } from "../type/force";
 
-import styles from '../style/ChargeModal.module.css';
+import styles from "../style/ChargeModal.module.css";
 
 export enum Mode {
-  Add, Edit
+  Add,
+  Edit,
 }
 
 interface ChargeModalProps {
@@ -32,32 +33,35 @@ export interface NewCharge {
 
 const modalStyle = {
   content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    display: 'flex',
-    gap: '50px',
-    padding: '30px'
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    display: "flex",
+    gap: "36px",
+    padding: "24px",
+    border: 'none',
+    borderRadius: '16px'
+  },
+  overlay: {
+    background: 'rgba(33, 33, 33, 0.5)'
   }
 };
 
 const ChargeModal: React.FC<ChargeModalProps> = (props) => {
-  const [charge, setCharge] = useState<NewCharge>(
-    {
-      name: props.charge.name,
-      y: props.charge.y,
-      x: props.charge.x,
-      color: props.charge.color,
-      charge: props.charge.charge
-    }
-  )
+  const [charge, setCharge] = useState<NewCharge>({
+    name: props.charge.name,
+    y: props.charge.y,
+    x: props.charge.x,
+    color: props.charge.color,
+    charge: props.charge.charge,
+  });
 
   useEffect(() => {
-    setCharge(props.charge)
-  }, [props.charge])
+    setCharge(props.charge);
+  }, [props.charge]);
 
   return (
     <ReactModal
@@ -65,64 +69,66 @@ const ChargeModal: React.FC<ChargeModalProps> = (props) => {
       onRequestClose={props.closeModal}
       style={modalStyle}
     >
-
       <div className={styles.container}>
-
-        <div className={styles.inputContainer} style={{ width: '205px' }}>
-          <p className='bold'>Charge name</p>
+        <div className={styles.inputContainer} style={{ width: "205px" }}>
+          <p className="bold">Charge name</p>
           <InputText
             value={charge.name}
-            placeholder={'Charge name'}
+            placeholder={"Charge name"}
             setValue={(value) => {
               charge.name = value;
-              setCharge({ ...charge })
+              setCharge({ ...charge });
             }}
           />
-
         </div>
         <div className={styles.inputContainer}>
-          <p className='bold'>X-value (cm)</p>
+          <p className="bold">X-value (cm)</p>
           <SliderInput
             value={charge.x}
             setValue={(x) => {
               charge.x = x;
-              setCharge({ ...charge })
+              setCharge({ ...charge });
             }}
           />
         </div>
         <div className={styles.inputContainer}>
-          <p className='bold'>Y-value (cm)</p>
+          <p className="bold">Y-value (cm)</p>
           <SliderInput
             value={charge.y}
             setValue={(y) => {
               charge.y = y;
-              setCharge({ ...charge })
+              setCharge({ ...charge });
             }}
           />
         </div>
 
         <div className={styles.inputContainer}>
-          <p className='bold'>Q value (μC) </p>
+          <p className="bold">Q value (μC) </p>
           <SliderInput
             value={charge.charge}
             setValue={(newCharge) => {
               charge.charge = newCharge;
-              setCharge({ ...charge })
+              setCharge({ ...charge });
             }}
             min={-10}
             max={10}
           />
         </div>
 
-        <div
-          style={{ display: "flex", justifyContent: "flex-end" }}
-        >
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
           <Button
             text={props.mode === Mode.Add ? "Add new charge" : "Save"}
-            icon={props.mode === Mode.Add ? "plus" : ''}
+            icon={props.mode === Mode.Add ? "plus" : ""}
             onClick={() => {
               const { name, x, y, color } = charge;
-              const newCharge = new Charge(name, charge.charge, x, y, color, new Force(0, 0, 0));
+              const newCharge = new Charge(
+                name,
+                charge.charge,
+                x,
+                y,
+                color,
+                new Force(0, 0, 0)
+              );
               props.onConfirm(newCharge);
               props.closeModal();
             }}
@@ -133,11 +139,14 @@ const ChargeModal: React.FC<ChargeModalProps> = (props) => {
       <div>
         <SketchPicker
           color={charge.color}
-          onChange={(color) => { charge.color = color.hex; setCharge({ ...charge }) }}
+          onChange={(color) => {
+            charge.color = color.hex;
+            setCharge({ ...charge });
+          }}
         />
       </div>
     </ReactModal>
-  )
-}
+  );
+};
 
 export default ChargeModal;
